d06edea66e09f7d9ccc21eff34828856999a0a56
mmarcosab <marcosab.vinicius@gmail.com>
2026-03-01 10:55:41 -0300

    estrutura inicial do projeto

---
+++ src/test/java/com/bank/clients/ClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
